aa392257719179b8b838fc80b7447c27b42e1704
jaredshito <raul_debek@hotmail.com>
2017-11-25 22:23:15 -0600

    plup
    login chingon

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const config = {
+  apiKey: "AIzaSyDkRbGa3v8O1YuTzChwDQxMFZ0oce-v0mE",
+  authDomain: "eat-time-8dbe0.firebaseapp.com",
+  databaseURL: "https://eat-time-8dbe0.firebaseio.com",
+  projectId: "eat-time-8dbe0",
+  storageBucket: "eat-time-8dbe0.appspot.com",
+  messagingSenderId: "592954673951"
+
+
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
+  ]
+})
+export class AppModule {}
